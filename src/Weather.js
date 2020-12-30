89ad2d8bb07b88ad2d6f2155cbc8fa9ba91ca3ee
Sourabh-Mandliya <mandliyasourabh@gmail.com>
2020-12-30 16:12:35 +0530

    weather project

---
+++ src/Weather.js
@@ -0,0 +1,98 @@
+import React,{useState, useEffect} from "react";
+import {Input,Button,Row,Col } from "reactstrap";
+import Axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
+
+function Weather(){
+    const [ weatherData, setWeatherData ] = useState({});
+    const [ cityName, setCityName ] = useState("delhi");
+    const [ message, setMessage ] = useState("");
+    const date = new Date();
+
+    const apiKey = "9062f34f404c7205cfd784137f7ee85d";
+    const notify = () => toast.warning(message,{hideProgressBar: true});
+    
+    useEffect(()=>{
+        findData();
+    },[]);
+
+    async function callApi(){
+       await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`).then(response =>{
+        setWeatherData(response.data);
+        console.log(response.data);
+       }).catch(err =>{
+           setMessage("Please search for a valid city 😩");
+           notify();
+           console.log(err);
+       })
+    }
+
+    function changeCityName(e){
+        setCityName(e.target.value);
+    }
+
+    function findData(){
+        callApi();
+    }
+
+    const details = !(Object.keys(weatherData).length === 0 && weatherData.constructor === Object) &&<div>
+        <h3>{weatherData.name}, {weatherData.sys.country}</h3>
+        <h6>{(date.getDate()+ "-" + (date.getMonth()+1)+ "-" +date.getFullYear())}</h6>  
+        <div>
+            <img 
+                src = {`https://openweathermap.org/img/wn/${weatherData.weather[0]["icon"]}@2x.png`} 
+                alt={weatherData.weather[0].main}
+                style={{minWidth:"10rem"}} 
+            />
+            <h5> {weatherData.weather[0].main}</h5>
+        </div>  
+        <div style={{display:"flex",justifyContent:"space-between"}}>
+            <div>
+                <h1 style={{fontSize:"3vw"}}>{weatherData.main.temp} <sup>o</sup>C</h1>
+            </div>
+            <div style={{fontSize:"1vw"}}>
+                <p>
+                    <strong> Low - High  
+                        <br/> 
+                        {weatherData.main.temp_min} <sup>o</sup>C - {weatherData.main.temp_max} <sup>&deg;</sup>C
+                        <br />
+                        Humidity : {weatherData.main.humidity}<br />
+                        Pressure : {weatherData.main.pressure} mb
+                    </strong>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    return (
+        <div style={{
+            backgroundColor:"#0A1172",
+            marginLeft:"25%",
+            marginRight:"25%",
+            marginTop:"2%",
+            fontFamily:"monospace",
+            color:"white", 
+            padding:"5%",
+            border:"1px solid black",
+            height:"50%",
+            width:"50%",
+            borderRadius:"20px"}}
+        >
+            <Row>
+                <Col>
+            <Input type="text" placeholder="Search city or state" onChange={(e)=>changeCityName(e)} ></Input>
+            <br />
+            <Button outline block type="submit" color="primary" onClick={()=>findData()}>Search</Button>
+            </Col>
+            </Row>
+            <br />
+            {details}
+            <ToastContainer />
+        </div>
+    )
+}
+
+
+export default Weather;
